7ba4af40f004e6da7c9b00e78489a3a9fc9d3dea
daniel faubel <drf821@gmail.com>
2026-02-25 14:48:03 -0700

    Persist shopping cart in Local Storage to retain data across sessions.

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,17 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
+
 
 const CartContext = createContext();
 
 export function CartProvider({ children }){
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(()=> {
+        const stored = localStorage.getItem('cart');
+        return stored ? JSON.parse(stored) : [];
+    });
+    
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
 
     const addToCart = (product) => {
         setCart((prev)=>{
@@ -14,7 +22,7 @@ export function CartProvider({ children }){
             return [...prev, {...product, qty: 1}]
         })
     }
-    
+
     const removeFromCart = (id)=> {
         setCart((prev) => prev.filter((item) => item.id !== id))
     }
