2a0eff34198b05800ddd9a10728f5e91dd2d5de3
daniel faubel <drf821@gmail.com>
2026-02-25 12:57:33 -0700

    Wrapped App.jsx in main.jsx with ProductContext to provide shared data without prop drilling.

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,15 @@
 import ProductCard from "./ProductCard";
+import { useProducts } from "../context/ProductContext";
 
-const ProductList = ({products}) => {
-    return ( 
-        
+const ProductList = () => {
+    const { products, loading, error } = useProducts();
+    return (     
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    {loading && <p>Loading...</p>}
+    {error && <div className='error'>{ error }</div>}
+    {products.map((product) => (
+      <ProductCard key={product.id} product={product} />
+    ))}
     </div>
      );
 }
