67fc895aa473e7533eb5083f73ef0af86136ab6b
daniel faubel <drf821@gmail.com>
2026-02-25 20:28:50 -0700

    Removed commented code. Cleaned spacing.

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,13 +5,13 @@ const ProductList = () => {
     const { products, loading, error } = useProducts();
     return (     
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
-    {loading && <p>Loading...</p>}
-    {error && <div className='error'>{ error }</div>}
-    {products.map((product) => (
-      <ProductCard key={product.id} product={product} />
-    ))}
+        {loading && <p>Loading...</p>}
+        {error && <div className='error'>{ error }</div>}
+        {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+        ))}
     </div>
-     );
+    );
 }
  
 export default ProductList;
--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -4,27 +4,7 @@ import data from "../data/db.json";
 const ProductContext = createContext();
 
 export function ProductProvider({children}){
-  //const [products, setProducts] = useState([]);
-  //const [loading, setLoading] = useState(true);
-  //const [error, setError] = useState(null);
 
- // useEffect(() =>{
-//     const fetchProducts = async () =>{
-//       try{
-//         //const res = await fetch(`${import.meta.env.VITE_API_URL}/products`);        if(!res.ok) throw new Error('Failed to fetch products');
-//         //const data = //await res.json();
-//         console.log(data);
-//         setProducts(data.products);
-//       }
-//       catch(err){
-//         setError(err.message);
-//       }
-//       finally{
-//         setLoading(false);
-//       }
-//     };
-//     fetchProducts();
-//   }, []);
   const [products] = useState(data.products);
   const loading = false;
   const error = null;
